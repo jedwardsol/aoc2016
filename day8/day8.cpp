ba2f3b3d444a1b67afa8b397d41b800608f25dc2
jedwards <jonathan_edwards@mcafee.com>
2021-12-28 12:22:32 -0800

    day8 parts 1&2

--- day8/day8.cpp
+++ day8/day8.cpp
@@ -119,10 +119,99 @@ auto readOperations()
 }
 
 
+using Screen = std::array<std::array<bool,50>,6>;
+
+
+void draw(Screen const &screen)
+{
+    for(auto row : screen)
+    {
+        for(auto pixel : row)
+        {
+            std::cout << (pixel ? '#' : ' ');
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n";
+}
+
+int count(Screen const &screen)
+{
+    int count{};
+
+    for(auto row : screen)
+    {
+        count+= std::ranges::count(row,true);
+    }
+
+    return count;        
+}
+
+
+
 int main()
 try
 {
     auto operations=readOperations();
+
+
+    Screen screen{};
+
+    for(auto const &operation : operations)
+    {
+        switch(operation.type)
+        {
+        case Operation::Type::Rect:
+        {
+            for(int row=0;row<operation.rect.height;row++)
+            {
+                for(int column=0;column<operation.rect.width;column++)
+                {
+                    screen[row][column]=true;
+                }
+            }
+        }
+        break;
+
+        case Operation::Type::RotateRow:
+        {
+            auto const row      = operation.rotate.offset;
+            auto const distance = operation.rotate.distance;
+
+            auto const originalRow=screen[row];
+            auto      &newRow=screen[row];
+
+            for(int column=0;column<50;column++)
+            {
+                newRow[column]=  originalRow[ (column+50 - distance) % 50];
+            }
+        }
+        break;
+
+
+        case Operation::Type::RotateColumn:
+        {
+            auto const column   = operation.rotate.offset;
+            auto const distance = operation.rotate.distance;
+
+            std::array<bool,6> originalColumn;
+
+            for(int row=0;row<6;row++)
+            {
+                originalColumn[row]=screen[row][column];
+            }
+
+            for(int row=0;row<6;row++)
+            {
+                screen[row][column]=  originalColumn[ (row+6 - distance) % 6];
+            }
+        }
+        break;
+        }
+    }
+    draw(screen);
+
+    std::cout << std::format("Part 1 : {}\n",count(screen));
 
     return 0;
 }
